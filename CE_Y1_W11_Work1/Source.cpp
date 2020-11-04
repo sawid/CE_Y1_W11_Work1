582184c20a0593e058686c9ddba139e6a0396144
sawid <zasawidza@gmail.com>
2020-11-05 01:21:09 +0700

    Add project files.

---
+++ CE_Y1_W11_Work1/Source.cpp
@@ -0,0 +1,29 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+void checkLetter(char);
+
+int main() {
+	char str[1000];
+	int i = 0;
+	int upper = 0, lower = 0;
+	scanf("%[^\n]", &str);
+	while (str[i] != '\0')
+	{
+		checkLetter(str[i],&upper,&lower);
+		i++;
+	}
+	printf("%d %d", upper, lower);
+}
+
+void checkLetter(char str,int* upper,int* lower) {
+	if (str >= 'A' && str <= 'Z')
+	{
+		upper++;
+	}
+	else if (str >= 'a' && str <= 'z')
+	{
+		lower++;
+	}
+}
+
